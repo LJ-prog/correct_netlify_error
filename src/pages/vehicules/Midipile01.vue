b8be4e48b66ce7ed74cc107f724f2b48db7231c3
guital <guillaumetaliercio@yahoo.fr>
2021-12-02 22:36:39 +0100

    #30 Dashboard stat vehicule

--- src/pages/vehicules/Midipile01.vue
+++ src/pages/vehicules/Midipile01.vue
@@ -1,33 +1,81 @@
 <template>
- 
-  <q-page class="q-pa-sm">
-    <div class="row q-col-gutter-sm q-py-sm">
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+  <q-page>
+    <div class="q-pa-md row q-gutter-md">
+
+      <div class="col-xs-12 col-sm-5 col-md-3">
+        <div class="text-h4 text-grey-8">Midipile 01</div>
         <q-card class="my-card">
           <q-img src="/statics/images/M1.JPG"/>
         </q-card>
       </div>
       
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-        <apex-chart></apex-chart>
+      <div class="col-xs-12 col-sm-6 col-md-3">
+        <q-card flat >
+            <q-card-section>
+              <table-actu></table-actu>
+            </q-card-section>
+          </q-card>
+        </div>
+
+
+      <div class="col-xs-12 col-sm-5 col-md-2">
+        <q-card flat >
+          <q-card-section>
+            <battery-radial 
+              :batteryvalue=[85]
+              :batterylabel="['Mobility ECO Score']"
+              :colormin="['#1a7a30']"
+              :colormax="['#1a7a30']"
+            ></battery-radial>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-xs-12 col-sm-5 col-md-2">
+        <q-card flat >
+          <q-card-section>
+            <battery-radial 
+              :batteryvalue=[85]
+              :batterylabel="['Midipile Challenge']"
+              :colormin="['#103e47']"
+              :colormax="['#103e47']"
+            ></battery-radial>
+          </q-card-section>
+        </q-card>
+
+      </div>
+
+    </div>
+
+    <div class="q-pa-md row q-gutter-md">
+      <div class="col-xs-12 col-sm-5 col-md-5">
+        <apex-stacked-column></apex-stacked-column>
+      </div>
+
+      <div class="col-xs-12 col-sm-5 col-md-5">
+        <apex-basic-line></apex-basic-line>
       </div>
     </div>
+
   </q-page>
 </template>
 
 <script>
-import {defineComponent, defineAsyncComponent} from 'vue';
+import { defineComponent, defineAsyncComponent } from 'vue'
 
 export default defineComponent({
-  name: "Charts",
+  name: 'PageIndex',
   components: {
-    ApexChart: defineAsyncComponent(() => import('components/charts/ApexChartRef')),
-  },
+    TableActu: defineAsyncComponent(() => import('components/tables/TableActu')),
+    BatteryRadial: defineAsyncComponent(() => import('components/charts/BatteryRadial.vue')),
+    ApexStackedColumn: defineAsyncComponent(() => import('components/charts/StackedColumn.vue')),
+    ApexBasicLine: defineAsyncComponent(() => import('components/charts/BasicLine.vue'))
+},
+  data () {
+    return {
+      loading: true,
+      dialog: true,
+    }
+  }
 })
 </script>
-
-<style lang="sass" scoped>
-.my-card
-  width: 80%
-  max-width: 400px
-</style>
